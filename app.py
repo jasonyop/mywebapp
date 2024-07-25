d1758e0531ebe9facfed09db7bd36a7d3fd64131
yop <yopjason@gmail.com>
2024-07-26 01:54:32 +0800

    v2 app

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
+import psutil
+import platform
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+    os_information = platform.platform()
+    cpu = psutil.cpu_percent(interval=1)
+    memory = psutil.virtual_memory().percent
+
+    return render_template('index.html', os_information=os_information, cpu=cpu, memory=memory)
+
+app.run(host='0.0.0.0', port=5000)
